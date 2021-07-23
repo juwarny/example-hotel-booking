053dd7eaef77befed1f499400f5f93cbfacdd112
Juwarny <4000w@naver.com>
2021-07-23 15:39:19 +0900

    merge apps

---
+++ frontend/src/components/MyPage.vue
@@ -0,0 +1,47 @@
+<template>
+
+<v-data-table
+    :headers="headers"
+    :items="myPage"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+
+</template>
+
+<script>
+  const axios = require('axios').default;
+
+  export default {
+    name: 'MyPage',
+    props: {
+      value: Object,
+      editMode: Boolean,
+      isNew: Boolean
+    },
+    data: () => ({
+        headers: [
+            { text: "id", value: "id" },
+            { text: "bookId", value: "bookId" },
+            { text: "hostId", value: "hostId" },
+            { text: "price", value: "price" },
+            { text: "startDate", value: "startDate" },
+            { text: "endDate", value: "endDate" },
+            { text: "roomId", value: "roomId" },
+            { text: "status", value: "status" },
+            { text: "payId", value: "payId" },
+            { text: "guestId", value: "guestId" },
+        ],
+        myPage : [],
+    }),
+    async created() {
+      var temp = await axios.get(axios.backend + '/mypages')
+
+      this.myPage = temp.data._embedded.mypages;
+
+    },
+    methods: {
+    }
+  }
+</script>
+
